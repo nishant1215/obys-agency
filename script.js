b36a44124a9ae31f0949f2ce13d1a2dbb40d631e
Nishant kumar yadav <nishuraj@Nishants-MacBook-Air.local>
2025-04-12 00:54:35 +0530

    page1 done

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ tl.to(".line h2",{
 tl.to("#loader",{
     opacity:0,
     duration:0.2,
-    delay:4,
+    delay:2,
 })
 tl.from("#page1",{
     delay:0.2,
@@ -39,6 +39,15 @@ tl.from("#page1",{
 })
 tl.to("#loader",{
     display:"none",
+})
+tl.from("#nav",{
+    y:-100,
+    opacity:0,
+    
+})
+tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1",{
+    y:100,
+    stagger:0.2,
 })
 
 }
